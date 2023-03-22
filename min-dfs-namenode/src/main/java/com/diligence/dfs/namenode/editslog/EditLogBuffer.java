96909b5ab10c1e41342cad83376a6e87cdf15f30
0 <0@0>
2023-03-23 01:00:02 +0800

    feat:缓冲区获取log

--- min-dfs-namenode/src/main/java/com/diligence/dfs/namenode/editslog/EditLogBuffer.java
+++ min-dfs-namenode/src/main/java/com/diligence/dfs/namenode/editslog/EditLogBuffer.java
@@ -2,6 +2,8 @@ package com.diligence.dfs.namenode.editslog;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Diligence
@@ -25,5 +27,20 @@ public class EditLogBuffer {
 		endTxid = editLog.getTxId();
 		// 将一条数据写入到先前定义的字节数组输出流
 		buffer.write(editLog.toByteArray());
+	}
+
+	/**
+	 * 获取当前缓冲区的 EditLog
+	 *
+	 * @return 当前缓冲区的EditLog
+	 */
+	public List<EditLogWrapper> getCurrentEditLog() {
+		// 将字节数组输出流转为字符数据
+		byte[] bytes = buffer.toByteArray();
+		if (bytes.length == 0) {
+			return new ArrayList<>();
+		}
+		// 解析字符数组，返回 editLog
+		return EditLogWrapper.parseFrom(bytes);
 	}
 }
--- min-dfs-namenode/src/main/java/com/diligence/dfs/namenode/editslog/EditLogWrapper.java
+++ min-dfs-namenode/src/main/java/com/diligence/dfs/namenode/editslog/EditLogWrapper.java
@@ -3,14 +3,19 @@ package com.diligence.dfs.namenode.editslog;
 import com.ruyuan.dfs.common.utils.ByteUtil;
 import com.ruyuan.dfs.common.utils.PrettyCodes;
 import com.ruyuan.dfs.model.backup.EditLog;
+import lombok.extern.slf4j.Slf4j;
 
+import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Diligence
  * @create 2023 - 03 - 23 0:43
  */
+@Slf4j
 public class EditLogWrapper {
 	// EditLog ：proto 生成的对象
 	private EditLog editLog;
@@ -48,6 +53,27 @@ public class EditLogWrapper {
 		byte[] ret = new byte[body.length + 4];
 		ByteUtil.setInt(ret, 0, bodyLength);
 		System.arraycopy(body, 0, ret, 4, bodyLength);
+		return ret;
+	}
+
+	public static List<EditLogWrapper> parseFrom(byte[] bytes) {
+		ByteBuffer byteBuffer = ByteBuffer.wrap(bytes);
+		return parseFrom(byteBuffer);
+	}
+
+	public static List<EditLogWrapper> parseFrom(ByteBuffer byteBuffer) {
+		List<EditLogWrapper> ret = new LinkedList<>();
+		while (byteBuffer.hasRemaining()) {
+			try {
+				int bodyLength = byteBuffer.getInt();
+				byte[] body = new byte[bodyLength];
+				byteBuffer.get(body);
+				EditLog editLog = EditLog.parseFrom(body);
+				ret.add(new EditLogWrapper(editLog));
+			} catch (Exception e) {
+				log.error("Parse EditLog failed.", e);
+			}
+		}
 		return ret;
 	}
 }
